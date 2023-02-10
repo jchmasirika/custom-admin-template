4f165385fbb0a1c4abbc760b71f5b686ba3442c2
jchmasirika <groupeaps.dev@gmail.com>
2023-02-11 01:28:59 +0200

    Ajout d'un header avec grillage mui

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Button, Layout, theme } from 'antd';
+import { Button, Layout, Popover, theme, Typography } from 'antd';
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined
 } from '@ant-design/icons';
+import { Grid, IconButton } from '@mui/material';
+import { Person2Outlined } from '@mui/icons-material';
 
 const MyHeader: React.FC<{ open: boolean, handleToggle: () => void }> = ({ open, handleToggle }) => {
     const {
@@ -12,8 +14,25 @@ const MyHeader: React.FC<{ open: boolean, handleToggle: () => void }> = ({ open,
 
     return (
         <Layout.Header style={{ padding: 0, background: colorBgContainer }}>
-            <Button style={{ margin: 10, border: 0 }} icon={ open ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />} onClick={handleToggle} />
+            <Grid container>
+                <Grid item xs={4}>
+                    <Button style={{ margin: 10, border: 0 }} icon={ open ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />} onClick={handleToggle} />
+                </Grid>
+                <Grid item xs={8} sx={{ textAlign: 'end' }}>
+                    <Popover content={<Account />} trigger='click' placement='bottom'>
+                        <IconButton sx={{ paddingRight: 2 }}>
+                            <Person2Outlined />
+                        </IconButton>
+                    </Popover>
+                </Grid>
+            </Grid>
         </Layout.Header>
+    );
+};
+
+const Account: React.FC = () => {
+    return (
+        <Typography>Content</Typography>
     );
 };
 
